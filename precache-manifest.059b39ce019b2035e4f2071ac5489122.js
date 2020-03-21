91465769ded325fa1c52639e7ce29bf4203249d5
Martin Zvozil <martin.zvozil@gmail.com>
2020-03-21 22:00:23 +0100

    Updates

---
+++ precache-manifest.059b39ce019b2035e4f2071ac5489122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63a669663f20a102cc6a8c6b33ab283",
+    "url": "/Demo-cypress/index.html"
+  },
+  {
+    "revision": "4c87a904143e9122a259",
+    "url": "/Demo-cypress/static/css/main.e917693d.chunk.css"
+  },
+  {
+    "revision": "dd6d79420857c0bd96ed",
+    "url": "/Demo-cypress/static/js/2.2780c80b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Demo-cypress/static/js/2.2780c80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c87a904143e9122a259",
+    "url": "/Demo-cypress/static/js/main.654f50af.chunk.js"
+  },
+  {
+    "revision": "d5ded57b19b81ea47e4d",
+    "url": "/Demo-cypress/static/js/runtime-main.6e715695.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Demo-cypress/static/media/logo.5d5d9eef.svg"
+  }
+]);
