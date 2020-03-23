53793731f7dccab4e96e6339e6ab9599896fddc9
Martin Zvozil <martin.zvozil@gmail.com>
2020-03-23 19:13:29 +0100

    all done

---
+++ src/components/about/about.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import "./about.styles.css";
+
+const About = () => {
+    return (
+        <div className="dumbassdiv">
+            <div className="aboutdiv">
+                <h1 className="abouth1">About</h1>
+                <p className="aboutp">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce wisi. Nullam dapibus fermentum ipsum. Donec iaculis gravida nulla. Etiam commodo dui eget wisi. Nullam faucibus mi quis velit. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Duis risus. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam.
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce wisi. Nullam dapibus fermentum ipsum. Donec iaculis gravida nulla. Etiam commodo dui eget wisi. Nullam faucibus mi quis velit. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Duis risus. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam.
+            </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
